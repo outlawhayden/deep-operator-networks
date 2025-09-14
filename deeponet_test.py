96823adb11a63c1c6b8c9166f2e944e37221042c
hayden outlaw <haydenkoutlaw@gmail.com>
2025-09-14 19:56:32 -0400

    deeponet training script

---
+++ deeponet_test.py
@@ -0,0 +1,178 @@
+import jax.numpy as jnp
+import numpy as np
+import jax
+from flax import nnx
+import optax
+from flax.training import train_state
+import scipy
+from scipy.spatial.distance import cdist
+from scipy.integrate import RK45
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import equinox as eqx
+from tqdm import tqdm
+
+seed = 42
+np.random.seed(42)
+key = jax.random.key(0)
+batch_size = 16
+num_branches = 10
+num_epochs = 10000
+
+
+# Linear Model (Wx + b)
+class Linear(eqx.Module):
+    weight: jax.Array
+    bias: jax.Array
+
+    def __init__(self, in_size, out_size, key):
+        wkey, bkey = jax.random.split(key)
+        self.weight = jax.random.normal(wkey, (out_size, in_size))
+        self.bias = jax.random.normal(bkey, (out_size))
+
+    def __call__(self, x):
+        return self.weight @ x + self.bias
+
+
+# Basic MultiLayer Perceptron Primitive
+class MLP(eqx.Module):
+    layers: list
+    activations: list
+
+    def __init__(self, architecture, key, activation = jax.nn.relu):
+        """
+        architecture: [in, hidden1, hidden2, ..., out]
+        key: random key
+        activation: jax.nn act fxn
+        """
+        keys = jax.random.split(key, len(architecture) - 1)
+        self.layers = [
+            Linear(architecture[i], architecture[i+1], keys[i]) for i in range(len(architecture) - 1)]
+        self.activations = [activation] * (len(self.layers) - 1) + [eqx.nn.Identity()] # no activation on last layer
+
+    def __call__(self, x):
+        for layer, act in zip(self.layers, self.activations):
+            x = act(layer(x))
+        return x
+
+# DeepONet Class Definition
+class DeepONet(eqx.Module):
+    branch: MLP
+    trunk: MLP
+
+    def __init__(self, branch_arch, trunk_arch, key, num_branches, activation=jax.nn.relu,): 
+        bkey, tkey = jax.random.split(key)
+        self.branch = MLP(branch_arch, bkey, activation)
+        self.trunk = MLP(trunk_arch, tkey, activation)
+
+    def __call__(self, x):
+        u, y = x
+        branch_out = self.branch(jnp.atleast_1d(u))
+        trunk_out = self.trunk(jnp.atleast_1d(y))
+        return jnp.inner(branch_out, trunk_out)
+
+
+# L2 loss function
+def loss_fn(model, x, y):
+    pred_y = jax.vmap(model)(x)
+    return jnp.mean((y - pred_y) ** 2)
+
+# load functional dataset
+print("loading dataset...")
+try:
+    dataset = np.load("integral_dataset.npy")
+    print("dataset loaded\n")
+except:
+    print("error loading dataset\n")
+
+# split into components
+u_all = dataset[:,:,1] # function at x_i
+x_all = dataset[:,:,0] # x_i sensor locations
+F_all = dataset[:,:,2] # target fxn, in this case also at x_i
+
+# total 1d x time mesh size
+n_traj, n_points = u_all.shape
+
+# make 1d x time mesh
+u_vectors = np.repeat(u_all, repeats = n_points, axis = 0)
+
+y_points = x_all.reshape(-1,1)
+targets = F_all.reshape(-1)
+
+# train_test_split using sklearn
+indices = np.arange(len(targets))
+train_idx, test_idx = train_test_split(indices, test_size = 0.33, random_state = seed)
+
+X_u_train, X_u_test = jnp.array(u_vectors[train_idx]), jnp.array(u_vectors[test_idx])
+X_y_train, X_y_test = jnp.array(y_points[train_idx]), jnp.array(y_points[test_idx])
+y_train, y_test = jnp.array(targets[train_idx]), jnp.array(targets[test_idx])
+
+
+# batching utility
+def get_batch(batch_size, U,Y,T,key):
+    key, subkey = jax.random.split(key)
+    idx = jax.random.choice(subkey, len(U), (batch_size,), replace = False)
+    return (U[idx], Y[idx]), T[idx], key
+
+# initialize training history
+train_hist = np.zeros(num_epochs)
+test_hist = np.zeros(num_epochs)
+
+# equinox training step 
+@eqx.filter_jit
+def train_step(model, opt_state, x, y, optimizer):
+    loss, grads = eqx.filter_value_and_grad(loss_fn)(model, x, y) # get loss and gradients
+    updates, opt_state = optimizer.update(grads, opt_state, model) # update step
+    model = eqx.apply_updates(model, updates) # apply updates
+    return model, opt_state, loss
+
+# testing loss step, jit wrap on loss_fn
+@eqx.filter_jit
+def eval_step(model, x, y): 
+    return loss_fn(model, x, y)
+
+u_dim = u_vectors.shape[1]
+y_dim = 1
+# define architectures for component networks
+branch_arch = [u_dim, 64, 64, num_branches]
+trunk_arch = [y_dim, 64, 64, num_branches]
+
+# model definition
+print(f"initializing model, b_arch: {branch_arch}, t_arch: {trunk_arch}, num_branches: {num_branches}")
+try:
+    model = DeepONet(branch_arch = branch_arch, trunk_arch = trunk_arch, num_branches = num_branches, key = key)
+    print("model initialized\n")
+except:
+    print("error initializing model\n")
+# optimizer - adam optax builtin
+optimizer = optax.adam(1e-3)
+opt_state = optimizer.init(eqx.filter(model, eqx.is_inexact_array))
+
+train_hist, test_hist = [],[]
+
+# training loop, wrapped in tqdm
+print("beginning training...")
+for epoch in tqdm(range(num_epochs)):
+    # using batch utility
+    (u_batch, y_batch), t_batch, key = get_batch(batch_size, X_u_train, X_y_train, y_train, key)
+    # step model
+    model, opt_state, train_loss = train_step(model, opt_state, (u_batch, y_batch), t_batch, optimizer)
+
+    # calculate and log losses
+    test_loss = eval_step(model, (X_u_test, X_y_test), y_test)
+
+    train_hist.append(float(train_loss))
+    test_hist.append(float(test_loss))
+
+print(test_hist[-1])
+print("training complete\n")
+# serialize leaves via equinox
+print("saving out...")
+try:
+    eqx.tree_serialise_leaves("deeponet_model.eqx", model)
+    print("model saved to deeponet_model.eqx")
+except:
+    print("error exporting, model not saved")
+
+# package export loss values
+np.savez("deeponet_losses.npz", train=np.array(train_hist), test=np.array(test_hist))
---
+++ functional_dataset_gen.py
@@ -0,0 +1,78 @@
+import jax.numpy as jnp
+import numpy as np
+import jax
+from flax import nnx
+import optax
+from flax.training import train_state
+import scipy
+from scipy.spatial.distance import cdist
+from scipy.integrate import RK45
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+from scipy.interpolate import interp1d
+from scipy.integrate import cumulative_trapezoid
+
+seed = 42
+np.random.seed(42)
+n_traj = 1000 # number of initial conditions
+n_points = 100 # number of samples on domain
+length_scale = 0.25 # smoothness of ICs
+variance = 2.0 # variance of ICs
+x_left = 0
+x_right = 1
+
+
+print("Number of Trajectories:", n_traj)
+print("Number of Samples/Trajectory:", n_points)
+print("Dataset Size:", n_traj * n_points)
+
+
+# kernel for gaussian random field function
+def rbf_kernel(X, Y, length_scale = 1.0, variance = 1.0):
+    dists = cdist(X, Y, metric = 'sqeuclidean')
+    return variance * np.exp(-0.5 * dists/ length_scale ** 2)
+
+# sample from gaussian random field, how you generate random initial conditions
+# n_points = smoothness on domain [x_left,x_right], length_scale controls smoothness
+def sample_grf(n_points = 100, n_dims = 1, length_scale = 1.0, variance = 1.0, n_samples = 5, random_state = seed):
+    rng = np.random.default_rng(random_state)
+    if n_dims == 1:
+        X = np.linspace(x_left, x_right, n_points)[:, None]
+    else:
+        X = rng.uniform(x_left, x_right, size = (n_points, n_dims))
+
+    K = rbf_kernel(X,X, length_scale = length_scale, variance = variance)
+    samples = rng.multivariate_normal(mean = np.zeros(n_points), cov = K, size = n_samples)
+    return X, samples
+
+# samples are u(x)
+x, samples = sample_grf(n_points = n_points, n_dims = 1, length_scale= length_scale, variance = variance, n_samples = n_traj, random_state = seed)
+x = x.reshape(-1)
+
+integrals = np.zeros_like(samples) # preallocate memory
+
+# use cumulative trapezoid function to approximate int_0^x u(x) = F
+# could be vectorized?
+for i in range(n_traj):
+    u = samples[i]
+    F = cumulative_trapezoid(u,x, initial = 0)
+    integrals[i] = F
+
+print("Samples shape:", samples.shape)
+print("Integrals shape:", integrals.shape)
+
+# Repeat x across all trajectories: shape (n_traj, n_points)
+x_tiled = np.tile(x, (n_traj, 1))
+
+# Stack along the last axis → (n_traj, n_points, 3)
+dataset = np.stack([x_tiled, samples, integrals], axis=-1)
+print("Total Dataset Shape:", dataset.shape)
+np.save('integral_dataset.npy', dataset) # needs to be .npy because dataset is 3d, otherwise ravel to csv
+print("Saved to: integral_dataset.npy")
+print("Done")
+
+"""
+axis 0 (index i, length n_traj: trajectory number (which IC))
+axis 1 (index j, length n_points): position on domain 
+axis 2 (index k): which attribute. 0 = x_k, 1 = u_i(x_k), 2 = F_i(x_j)
+"""
